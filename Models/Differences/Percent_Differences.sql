0775d2b8619b91b9b3efa1e4086c59f13d7d3a40
Eris-Totle <114885314+Eris-Totle@users.noreply.github.com>
2025-03-23 10:42:40 -0400

    Add files via upload

---
+++ Models/Differences/Percent_Differences.sql
@@ -0,0 +1,25 @@
+WITH Percent_Differences AS
+(
+    SELECT 
+        "REGION", 
+        "STATE", 
+        "SEX", 
+        "ORIGIN", 
+        "RACE", 
+        "AGE", 
+        SUM(("POPESTIMATE2021" - "POPESTIMATE2020") / NULLIF("POPESTIMATE2020", 0) * 100) AS Percent_Change_2021_2020, 
+        SUM(("POPESTIMATE2022" - "POPESTIMATE2021") / NULLIF("POPESTIMATE2021", 0) * 100) AS Percent_Change_2022_2021, 
+        SUM(("POPESTIMATE2023" - "POPESTIMATE2022") / NULLIF("POPESTIMATE2022", 0) * 100) AS Percent_Change_2023_2022, 
+        SUM(("POPESTIMATE2023" - "POPESTIMATE2020") / NULLIF("POPESTIMATE2020", 0) * 100) AS Percent_Change_2023_2020 
+    FROM 
+        {{ ref('Population_Prediction') }}  -- Reference your base table or another model
+    GROUP BY 
+        "REGION", 
+        "STATE", 
+        "SEX", 
+        "ORIGIN", 
+        "RACE", 
+        "AGE"
+)
+
+SELECT * FROM Percent_Differences;
