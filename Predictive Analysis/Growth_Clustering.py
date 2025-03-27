6d5b480720a67ba5597621d5eff0016617c6d1e5
Eris-Totle <114885314+Eris-Totle@users.noreply.github.com>
2025-03-27 13:24:01 -0400

    Create Growth_Clustering.py

---
+++ Predictive Analysis/Growth_Clustering.py
@@ -0,0 +1,12 @@
+# In progress - Initial clustering for growth analysis using k - prototypes...
+
+from kmodes.kprototypes import KPrototypes
+
+X = df[['REGION', 'STATE', 'growth_2021_2020', 'growth_2022_2021', 'growth_2023_2022']].values
+
+kproto = KPrototypes(n_clusters=3, init='Cao', verbose=2)
+clusters = kproto.fit_predict(X, categorical=[0, 1])  # 0 and 1 are the indices of categorical columns
+
+df['cluster'] = clusters
+
+print(df)
