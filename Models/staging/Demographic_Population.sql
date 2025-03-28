1871314b29bd2429fac6f9163de7d9a8ad46a5f0
Eris-Totle <114885314+Eris-Totle@users.noreply.github.com>
2025-03-27 20:14:58 -0400

    Update Demographic_Population.sql

--- Models/staging/Demographic_Population.sql
+++ Models/staging/Demographic_Population.sql
@@ -1,1 +1,15 @@
-
+WITH demographic_estimates AS (
+  SELECT 
+    "AGE", 
+    "RACE",
+    "ORIGIN",
+    "SEX",
+    "ESTIMATEBASE2020", 
+    "POPULATIONESTIMATE2020", 
+    "POPULATIONESTIMATE2021", 
+    "POPULATIONESTIMATE2022", 
+    "POPULATIONESTIMATE2023"
+  FROM {{ ref('Population_Prediction') }}
+)
+SELECT * 
+FROM demographic_estimates;
