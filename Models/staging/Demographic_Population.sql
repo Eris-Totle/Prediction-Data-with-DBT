89f6860e75df0acc2f732403ac13580aa57262c4
Eris-Totle <114885314+Eris-Totle@users.noreply.github.com>
2025-04-10 18:34:48 -0400

    Update Demographic_Population.sql

--- Models/staging/Demographic_Population.sql
+++ Models/staging/Demographic_Population.sql
@@ -1,15 +1,31 @@
 WITH demographic_estimates AS (
   SELECT 
-    "AGE", 
-    "RACE",
-    "ORIGIN",
-    "SEX",
+    "AGE",
+    CASE
+        WHEN "RACE"=1 THEN 'White'
+        WHEN "RACE"=2 THEN 'African_American_Black'
+        WHEN "RACE"=3 THEN 'American_Indian_or_Alaska_Native'
+        WHEN "RACE"=4 THEN 'Asian'
+        WHEN "RACE"=5 THEN 'Native_Hawaiian_or_Other_Pacific_Islander'
+        WHEN "RACE"=6 THEN 'Two_or_more_races'
+        END AS RACE_CAT,
+    CASE
+        WHEN "ORIGIN"=0 THEN 'Total'
+        WHEN "ORIGIN"=1 THEN 'Not_Hispanic'
+        WHEN "ORIGIN"=2 THEN 'HISPANIC'
+        END AS ORIGIN_CAT,
+    CASE 
+        WHEN "SEX"=0 THEN 'Total'
+        WHEN "SEX"=1 THEN 'Male' 
+        WHEN "AGE"=2 THEN 'Female'
+        END AS SEX_CAT,
     "ESTIMATESBASE2020", 
     "POPESTIMATE2020", 
     "POPESTIMATE2021", 
-    "POESTIMATE2022", 
+    "POPESTIMATE2022", 
     "POPESTIMATE2023"
-  FROM {{ ref('Population_Prediction') }} ##running this outside of the staging process - you can replace with "Population_Predictions", or whatever you decide to sent it to postgres as.
+  FROM {{ ref('Population_Prediction') }} ## for folks interested in running this outside of the staging process - you can replace with "Population_Predictions", or whatever you decide to sent it to postgres as.
+)
 )
 SELECT * 
-FROM demographic_estimates;
+FROM demographic_estimates
