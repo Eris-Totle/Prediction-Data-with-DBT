0dcbfac381e7e9fe506d021a7a5b70ecf62b5dcb
Eris-Totle <114885314+Eris-Totle@users.noreply.github.com>
2025-04-20 12:34:01 -0400

    Update growth_trends.sql

--- Models/Marts/Growth_Analysis/growth_trends.sql
+++ Models/Marts/Growth_Analysis/growth_trends.sql
@@ -3,20 +3,20 @@
 WITH state_totals AS (
     SELECT 
         "REGION", 
-        "STATE", 
+        "NAME", 
         SUM("POPESTIMATE2020") AS pop_2020,
         SUM("POPESTIMATE2021") AS pop_2021,
         SUM("POPESTIMATE2022") AS pop_2022,
         SUM("POPESTIMATE2023") AS pop_2023
     FROM "Population_Prediction"
-    WHERE "SEX" = 0 AND "ORIGIN" = 0 # -modify this as needed pending your analysis needs, I ran into the issue of doubling values without filtering. 
-    GROUP BY "REGION", "STATE"
+    WHERE "SEX" = 0 AND "ORIGIN" = 0
+    GROUP BY "REGION", "NAME"
 ),
 
 growth_trends AS (
     SELECT 
         "REGION", 
-        "STATE", 
+        "NAME", 
         (pop_2021 - pop_2020) / NULLIF(pop_2020, 0) * 100 AS growth_2021_2020,
         (pop_2022 - pop_2021) / NULLIF(pop_2021, 0) * 100 AS growth_2022_2021,
         (pop_2023 - pop_2022) / NULLIF(pop_2022, 0) * 100 AS growth_2023_2022
@@ -24,4 +24,3 @@ growth_trends AS (
 )
 
 SELECT * FROM growth_trends;
-
