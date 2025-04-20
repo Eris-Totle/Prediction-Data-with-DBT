e1e17301395311d88ff0e6cbf854447f4fd4fce9
Eris-Totle <114885314+Eris-Totle@users.noreply.github.com>
2025-04-20 12:36:26 -0400

    Update Growth_Clustering.py

--- Predictive Analysis/Growth_Clustering.py
+++ Predictive Analysis/Growth_Clustering.py
@@ -1,5 +1,29 @@
 # In progress - Initial clustering for growth analysis using k - prototypes...
 
+import pandas as pd
+from sqlalchemy import create_engine
+
+# PostgreSQL connection string from DBT example
+db_connection_url = "(Use your postgres creds)"
+
+# Create SQLAlchemy engine
+engine = create_engine(db_connection_url)
+
+# Test connection and list the tables
+tables = pd.read_sql("SELECT table_name FROM information_schema.tables WHERE table_schema='public'", engine)
+print(tables)
+
+# Query sales data
+growth_query = '''
+SELECT*FROM growth_trends;
+'''
+df = pd.read_sql(growth_query, engine)
+
+# Display the first few rows
+print(df.head())
+
+df['REGION']=df['REGION'].astype('category')
+
 from kmodes.kprototypes import KPrototypes
 
 X = df[['REGION', 'STATE', 'growth_2021_2020', 'growth_2022_2021', 'growth_2023_2022']].values
