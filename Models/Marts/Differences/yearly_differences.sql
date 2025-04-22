da32dd572d4fb2943aca6e7be100a9491944718b
Eris-Totle <114885314+Eris-Totle@users.noreply.github.com>
2025-04-22 12:42:18 -0400

    Update yearly_differences.sql

--- Models/Marts/Differences/yearly_differences.sql
+++ Models/Marts/Differences/yearly_differences.sql
@@ -12,7 +12,7 @@ WITH Yearly_Differences AS
         SUM("POPESTIMATE2023" - "POPESTIMATE2022") AS Diff_Between_2023_2022, 
         SUM("POPESTIMATE2023" - "POPESTIMATE2020") AS Total_difference 
     FROM 
-        {{ ref('Population_Prediction') }} 
+        {{ source('raw_data', 'Population_Prediction') }}
     GROUP BY 
         "REGION", 
         "STATE", 
