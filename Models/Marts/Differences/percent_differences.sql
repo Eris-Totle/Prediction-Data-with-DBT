602a9ae9080204c8eba8692e31d85dc31ad18a1a
Eris-Totle <114885314+Eris-Totle@users.noreply.github.com>
2025-04-22 12:42:28 -0400

    Update percent_differences.sql

--- Models/Marts/Differences/percent_differences.sql
+++ Models/Marts/Differences/percent_differences.sql
@@ -12,7 +12,7 @@ WITH Percent_Differences AS
         SUM(("POPESTIMATE2023" - "POPESTIMATE2022") / NULLIF("POPESTIMATE2022", 0) * 100) AS Percent_Change_2023_2022, 
         SUM(("POPESTIMATE2023" - "POPESTIMATE2020") / NULLIF("POPESTIMATE2020", 0) * 100) AS Percent_Change_2023_2020 
     FROM 
-        {{ ref('Population_Prediction') }}  
+        {{ source('raw_data', 'Population_Prediction') }}  
     GROUP BY 
         "REGION", 
         "STATE", 
